b8bf99ec109fdd878a1045080ff165a58e3886ad
vishalmishra4u <mishravishal0912@gmail.com>
2017-08-09 01:37:04 +0530

    view added

--- app.js
+++ app.js
@@ -2,3 +2,17 @@ var express = require('express'),
   path = require('path'),
   bodyParser = require('body-parser'),
   nodemailer = require('nodemailer');
+
+var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : false}));
+
+app.set('views',path.join(__dirname,'views'));
+app.set('view engine', 'jade');
+
+app.get('/',function(req, res){
+  res.render('index');
+});
+
+app.listen(3000);
+console.log('Server is running on port 3000');
